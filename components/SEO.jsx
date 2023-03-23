3399b55646a79aeb4e4711704cbba6d95f681fa9
Sami Warraich <samiwarraich51@gmail.com>
2023-03-24 00:49:43 +0300

    fixes

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -39,13 +39,13 @@ function SEO() {
       <link rel="icon" type="image/png" sizes="32x32" href="./favicon.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="./favicon.png" />
       {/* <!-- Clarity tracking code for https://samiwarraich.github.io/ --> */}
-      {/* <script>
+      <script>
         {`(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "${MS_CLARITY}");`}
-      </script> */}
+      </script>
     </Head>
   );
 }
