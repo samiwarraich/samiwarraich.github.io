8dd51e83b9ebbf94d6a459cac05c8247365be81c
Sami Warraich <samiwarraich51@gmail.com>
2023-03-26 21:15:11 +0300

    fixes

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -36,8 +36,8 @@ function SEO() {
       <link rel="manifest" href="/manifest.json" />
       {/* Favicon */}
       <link rel="apple-touch-icon" sizes="120x120" href="./favicon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="./favicon.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="./favicon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="favicon.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="favicon.png" />
       {/* <!-- Clarity tracking code for https://samiwarraich.github.io/ --> */}
       <script>
         {`(function(c,l,a,r,i,t,y){
