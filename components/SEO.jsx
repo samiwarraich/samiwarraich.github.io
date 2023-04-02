696682d249757c34eb24b638939984898417ff3b
Sami Warraich <samiwarraich51@gmail.com>
2023-04-02 16:18:53 +0300

    fixes

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -18,6 +18,7 @@ function SEO() {
         content="ELB_vUPdHzKluiiPGhpV3ORRmCo1ogHoTYwRMp6Bm54"
       />
       <meta name="msvalidate.01" content="DD992E9CC85A0E458EA3599D0526C78A" />
+      <meta name="msnbot" content="preview"></meta>
       <link rel="manifest" href="/manifest.json" />
       <link rel="canonical" href={seoData.url} />
 
