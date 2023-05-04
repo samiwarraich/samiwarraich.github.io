889cb557cb2344d0df01c758903555f024fe272b
Sami Warraich <samiwarraich51@gmail.com>
2023-05-04 20:38:26 +0300

    remove vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { Analytics } from "@vercel/analytics/react";
 import { analytics } from "../utils/firebase";
 import Script from "next/script";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,7 +15,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
       {/* <!-- Clarity tracking code for https://samiwarraich.github.io/ --> */}
       <Script id="ms-clarity">
         {`(function(c,l,a,r,i,t,y){
