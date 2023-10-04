c9811a3c608c1ad332d828547ce9f05b9d58674a
Sami Warraich <samiwarraich51@gmail.com>
2023-10-04 19:48:51 +0300

    fixes

--- components/EducationCard.jsx
+++ components/EducationCard.jsx
@@ -5,7 +5,7 @@ import Fade from "react-reveal/Fade";
 const EducationCard = ({ education }) => {
   return (
     <Fade left duration={2000}>
-      <Card className="card-lift--hover shadow mt-4">
+      <Card className="shadow mt-4">
         <CardBody>
           <div className="d-flex px-3">
             <div className="pl-4">
